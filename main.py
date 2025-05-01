f1b2fac421848e5af9a9ae04ac7fd67bd3d93949
glaremage <jrfabella@gmail.com>
2025-05-01 00:21:44 -0400

    added more books and didn't hard code the path

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_chars, sort
 
 def get_books(path):
@@ -5,15 +6,20 @@ def get_books(path):
         file_contents = f.read()
         return file_contents
     
+    
+    
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
     print("----------- Word Count ----------")
-    print(f"Found {count_words(get_books("./books/frankenstein.txt"))} total words")
+    print(f"Found {count_words(get_books(sys.argv[1]))} total words")
     print("--------- Character Count -------")
-    for items in sort(count_chars(get_books("./books/frankenstein.txt"))):
+    for items in sort(count_chars(get_books(sys.argv[1]))):
         if items["letter"].isalpha() == True:
             print(f"{items["letter"]}: {items["num"]}")
     print("============= END ===============")
